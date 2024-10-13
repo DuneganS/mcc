fa68b476b5a309812d4dc5c4d34a7429fb5fc37f
Stacy Dunegan <skdunegan@gmail.com>
2024-10-13 16:51:29 -0400

    Some formatting and ignoring when a negative value is used

--- app/page.tsx
+++ app/page.tsx
@@ -28,8 +28,12 @@ export default function Home() {
 
   const handleInputChange = useCallback(
     (id: string, amount: number | null) => {
+      if (amount !== null && amount < 0) {
+        return;
+      }
+
       setCraftingItems((prev) => {
-        if (amount === null) {
+        if (amount === null || amount === 0) {
           const rest = Object.fromEntries(
             Object.entries(prev).filter(([key]) => key !== id)
           );
@@ -59,42 +63,61 @@ export default function Home() {
 
   return (
     <main>
-      <div className="grid grid-cols-2 gap-4 p-4 bg-black">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 bg-gray-300 justify-center">
-          {items
-            .filter((item) => item.craftable)
-            .map((item) => (
-              <div key={item.id} className="bg-gray-300">
-                <ItemBox item={item} onInputChange={handleInputChange} />
-              </div>
-            ))}
+      {items.length === 0 ? (
+        <div className="flex justify-center items-center h-screen">
+          <span>
+            No craftable items. Go to the{" "}
+            <a className="text-blue-500 hover:underline" href="/items">
+              Items
+            </a>{" "}
+            page to add some
+          </span>
         </div>
-        <div className="bg-gray-300 p-4">
-          <h2 className="text-2xl font-bold text-center">
-            Required Ingredients
-          </h2>
-          <ul>
-            {Object.entries(requiredIngredients).map(([id, amount]) => {
-              const item = items.find((item) => item.id === id);
-              if (!item) return null;
-              return (
-                <li key={id} className="flex items-center gap-2">
-                  <Image
-                    src={item.image}
-                    alt={item.name}
-                    width={32}
-                    height={32}
-                  />
-                  <span>
-                    {item.name} x {amount} [{Math.floor(amount / 64)} +{" "}
-                    {amount % 64}]
-                  </span>
-                </li>
-              );
-            })}
-          </ul>
+      ) : (
+        <div className="grid grid-cols-2 gap-4 p-4 bg-black">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 p-4 bg-gray-300">
+            <h2 className="text-2xl font-bold text-center col-span-full">
+              Craftable Items
+            </h2>
+            {items
+              .filter((item) => item.craftable)
+              .map((item) => (
+                <div key={item.id} className="bg-gray-300">
+                  <ItemBox item={item} onInputChange={handleInputChange} />
+                </div>
+              ))}
+          </div>
+          <div className="bg-gray-300 p-4">
+            <h2 className="text-2xl font-bold text-center">
+              Required Ingredients
+            </h2>
+            <ul>
+              {Object.entries(requiredIngredients).map(([id, amount]) => {
+                const item = items.find((item) => item.id === id);
+                if (!item) return null;
+                return (
+                  <li
+                    key={id}
+                    className="flex items-center gap-2 bg-gray-400 rounded-lg p-2 w-max mb-2"
+                  >
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      width={32}
+                      height={32}
+                      className="rounded-full"
+                    />
+                    <span>
+                      {item.name} x {amount} [{Math.floor(amount / 64)} +{" "}
+                      {amount % 64}]
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
         </div>
-      </div>
+      )}
     </main>
   );
 }
